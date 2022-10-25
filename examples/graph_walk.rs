65e520308cac2bbe1d51674814f8b21d8674944b
michaelrauh <michaelrauh@ymail.com>
2022-10-24 21:55:38 -0400

    begin adding queue logic

--- examples/graph_walk.rs
+++ examples/graph_walk.rs
@@ -1,9 +1,10 @@
-use std::{collections::HashSet, io::Write};
+use amiquip::{AmqpValue, Connection, FieldTable, QueueDeclareOptions};
+use beanstalkc::{Beanstalkc, Job};
+use serde::{Deserialize, Serialize};
+use silkworm::{DataCycle, Registry};
 use std::fs::File;
 use std::process;
-use serde::{Deserialize, Serialize};
-use silkworm::Registry;
-
+use std::{collections::HashSet, io::Write};
 
 fn main() {
     println!("Hello from an example!");
@@ -11,13 +12,13 @@ fn main() {
 
 #[derive(PartialEq, Eq, Hash, Serialize, Deserialize, Default)]
 struct Node {
-    _label: String
+    _label: String,
 }
 
 #[derive(PartialEq, Eq, Hash, Serialize, Deserialize, Default)]
 struct Edge {
     from: Node,
-    to: Node
+    to: Node,
 }
 
 #[derive(PartialEq, Eq, Serialize, Deserialize, Default)]
@@ -25,15 +26,23 @@ struct GraphData {
     nodes: HashSet<Node>,
     edges: HashSet<Edge>,
     input_files: HashSet<String>,
-    paths: HashSet<Vec<Edge>>
+    paths: HashSet<Vec<Edge>>,
 }
 
-struct Holder{}
+impl DataCycle for Node {
+    type Database = GraphData;
+
+    fn stop_categorically(&self, db: Self::Database) -> bool {
+        !db.edges.is_empty()
+    }
+}
+
+struct Holder {}
 
 impl Registry for Holder {
-    
     type Database = GraphData;
     type Location = File;
+    type GlobalQueueLocation = Beanstalkc;
 
     fn create_db(&self) -> Self::Database {
         GraphData::default()
@@ -53,6 +62,24 @@ impl Registry for Holder {
         loc.write_all(&serialized)?;
         Ok(())
     }
+
+    fn create_global_queue(&self) -> Result<Self::GlobalQueueLocation, anyhow::Error> {
+        let conn = Beanstalkc::new()
+            .connect()
+            .expect("connect to beanstalkd server failed");
+        Ok(conn)
+    }
+
+    fn consume_global(&self, mut queue: Self::GlobalQueueLocation) -> Result<Vec<u8>, anyhow::Error> {
+        queue.watch("jobs").unwrap();
+
+        let job = queue.reserve().unwrap();
+
+        Ok(job.body().to_vec())
+        // add a delete call to lifecycle
+    }
+
+    
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
 pub trait Registry {
     type Database;
     type Location;
+    type GlobalQueueLocation;
     fn worker_name(&self) -> String;
     fn create_db(&self) -> Self::Database;
     fn db_location(&self, worker_name: String) -> Result<Self::Location, anyhow::Error>;
     fn write_db(&self, loc: Self::Location, db: Self::Database) -> Result<(), anyhow::Error>;
+    fn create_global_queue(&self) -> Result<Self::GlobalQueueLocation, anyhow::Error>;
+    fn consume_global(&self, queue: Self::GlobalQueueLocation) -> Result<Vec<u8>, anyhow::Error>;
+
+    // read/write (to, from) (global, local) queue
 }
 
 pub trait DataCycle {
     type Database;
+    // event name
     fn stop_categorically(&self, db: Self::Database) -> bool;
+}
+
+fn run_worker(reg: impl Registry, worker: impl DataCycle) {
+    todo!()
 }
 
 fn life(reg: impl Registry) -> Result<(), anyhow::Error> {
