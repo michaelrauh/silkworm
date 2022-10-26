91ddfde8195553cbdb7201212e227ddcf3340de5
michaelrauh <michaelrauh@ymail.com>
2022-10-26 19:50:35 -0400

    WIP start sketching out run worker logic. Lots of comments at the moment

--- examples/graph_walk.rs
+++ examples/graph_walk.rs
@@ -1,10 +1,11 @@
-use amiquip::{AmqpValue, Connection, FieldTable, QueueDeclareOptions};
-use beanstalkc::{Beanstalkc, Job};
+use beanstalkc::{Beanstalkc};
 use serde::{Deserialize, Serialize};
 use silkworm::{DataCycle, Registry};
+use std::collections::HashMap;
 use std::fs::File;
 use std::process;
-use std::{collections::HashSet, io::Write};
+use std::time::Duration;
+use std::{io::Write};
 
 fn main() {
     println!("Hello from an example!");
@@ -23,10 +24,16 @@ struct Edge {
 
 #[derive(PartialEq, Eq, Serialize, Deserialize, Default)]
 struct GraphData {
-    nodes: HashSet<Node>,
-    edges: HashSet<Edge>,
-    input_files: HashSet<String>,
-    paths: HashSet<Vec<Edge>>,
+    nodes: HashMap<i64, Node>,
+    edges: HashMap<i64, Edge>,
+    input_files: HashMap<i64, String>,
+    paths: HashMap<i64, Vec<Edge>>,
+}
+
+#[derive(PartialEq, Eq, Serialize, Deserialize, Default)]
+struct DatabaseLocation {
+    data_type: String,
+    hash: i64
 }
 
 impl DataCycle for Node {
@@ -43,6 +50,9 @@ impl Registry for Holder {
     type Database = GraphData;
     type Location = File;
     type GlobalQueueLocation = Beanstalkc;
+    type DataRoute = DatabaseLocation;
+    type JobReceipt = u64;
+    type LocalQueue = Vec<DatabaseLocation>;
 
     fn create_db(&self) -> Self::Database {
         GraphData::default()
@@ -65,20 +75,50 @@ impl Registry for Holder {
 
     fn create_global_queue(&self) -> Result<Self::GlobalQueueLocation, anyhow::Error> {
         let conn = Beanstalkc::new()
-            .connect()
-            .expect("connect to beanstalkd server failed");
+            .connect()?;
         Ok(conn)
     }
 
-    fn consume_global(&self, mut queue: Self::GlobalQueueLocation) -> Result<Vec<u8>, anyhow::Error> {
-        queue.watch("jobs").unwrap();
+    fn consume_global(&self, mut queue: Self::GlobalQueueLocation) -> Result<(Self::DataRoute, Self::JobReceipt), anyhow::Error> {
+        queue.watch("jobs")?;
 
-        let job = queue.reserve().unwrap();
+        let job = queue.reserve()?;
+        let ans = bincode::deserialize(job.body())?;
+        let reciept = job.id();
 
-        Ok(job.body().to_vec())
-        // add a delete call to lifecycle
+        Ok((ans, reciept))
     }
 
+    fn produce_global(&self, data: Self::DataRoute, mut queue: Self::GlobalQueueLocation, priority: usize) -> Result<Self::JobReceipt, anyhow::Error> {
+        let to_put = bincode::serialize(&data)?;
+        let res = queue.put(&to_put, priority.try_into().unwrap(), Duration::from_secs(0), Duration::from_secs(10))?;
+
+        Ok(res)
+    }
+
+    fn ack_global(&self, mut queue: Self::GlobalQueueLocation, receipt: Self::JobReceipt) -> Result<(), anyhow::Error> {
+        let _res = queue.delete(receipt)?;
+        Ok(())
+    }
+
+    fn nack_global(&self, mut queue: Self::GlobalQueueLocation, receipt: Self::JobReceipt) -> Result<(), anyhow::Error> {
+        queue.bury_default(receipt)?;
+        queue.kick_job(receipt)?;
+
+        Ok(())
+    }
+
+    fn create_local_queue(&self) -> Self::LocalQueue {
+        vec![]
+    }
+
+    fn consume_local(&self, mut queue: Self::LocalQueue) -> Self::DataRoute { // note there is no ack or nack for local as it consumes live and infallible
+        queue.pop().expect("should this method be optional?") // there is an issue here: framework should assume nonempty at this stage so the types should too
+    }
+
+    fn produce_local(&self, mut queue: Self::LocalQueue, loc: Self::DataRoute) {
+        queue.push(loc)
+    }
     
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,50 @@ pub trait Registry {
     type Database;
     type Location;
     type GlobalQueueLocation;
+    type DataRoute;
+    type JobReceipt;
+    type LocalQueue;
     fn worker_name(&self) -> String;
     fn create_db(&self) -> Self::Database;
     fn db_location(&self, worker_name: String) -> Result<Self::Location, anyhow::Error>;
     fn write_db(&self, loc: Self::Location, db: Self::Database) -> Result<(), anyhow::Error>;
     fn create_global_queue(&self) -> Result<Self::GlobalQueueLocation, anyhow::Error>;
-    fn consume_global(&self, queue: Self::GlobalQueueLocation) -> Result<Vec<u8>, anyhow::Error>;
-
-    // read/write (to, from) (global, local) queue
+    fn consume_global(&self, queue: Self::GlobalQueueLocation) -> Result<(Self::DataRoute, Self::JobReceipt), anyhow::Error>;
+    fn produce_global(&self, data: Self::DataRoute, queue: Self::GlobalQueueLocation, priority: usize) -> Result<Self::JobReceipt, anyhow::Error>;
+    fn ack_global(&self, queue: Self::GlobalQueueLocation, receipt: Self::JobReceipt) -> Result<(), anyhow::Error>;
+    fn nack_global(&self, queue: Self::GlobalQueueLocation, receipt: Self::JobReceipt) -> Result<(), anyhow::Error>;
+    fn create_local_queue(&self) -> Self::LocalQueue;
+    fn consume_local(&self, queue: Self::LocalQueue) -> Self::DataRoute;
+    fn produce_local(&self, queue: Self::LocalQueue, loc: Self::DataRoute);
 }
 
 pub trait DataCycle {
     type Database;
-    // event name
     fn stop_categorically(&self, db: Self::Database) -> bool;
 }
 
-fn run_worker(reg: impl Registry, worker: impl DataCycle) {
-    todo!()
-}
+fn run_worker(reg: impl Registry, worker: impl DataCycle) -> Result<(), anyhow::Error> { // question this returning result
+    // work out empty queue (waiting to start) scenario
 
-fn life(reg: impl Registry) -> Result<(), anyhow::Error> {
+
     let name = reg.worker_name();
     let db = reg.create_db();
     let loc = reg.db_location(name)?;
-    reg.write_db(loc, db)?;
+    let global_queue = reg.create_global_queue()?;
+
+    let (batch, receipt) = reg.consume_global(global_queue)?; // consume batch size events and repeat batch count times
+
+    // cycle data
+    // let (db_update, global_events) = cycle_data(db, worker, batch);
+    //
+    let db_update = todo!();
+    let global_events = todo!();
+
+    reg.write_db(loc, db_update)?;
+    reg.ack_global(global_queue, receipt)?;
+    // reg.nack_global(global_queue, receipt)?; // add this in somewhere
+    // produce a merge event on the global queue
+    
     Ok(())
 }
 
